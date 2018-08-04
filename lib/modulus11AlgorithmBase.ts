0ddda85b87f8c1055aff809ba4309442e8aab8f9
Unknown <stuart.clark@z.co.nz>
2018-08-05 09:05:54 +1200

    Impiment modulus algorithms

--- lib/modulus11AlgorithmBase.ts
+++ lib/modulus11AlgorithmBase.ts
@@ -3,30 +3,52 @@ export abstract class Modulus11AlgorithmBase {
 	protected modulus?: number;
 
 	validate(formattedAccountNumber: string): boolean {
-		const accountNumber = 0;// Regex.Replace(formattedAccountNumber, '-', '');
 
-		if (this.weightings.length !== 16) {
+		const accountNumber = formattedAccountNumber.replace('-', '');
+
+		if (!this.weightings || this.weightings.length !== 16) {
 			throw new Error('Invalid weightings');
 		}
 
-		if (accountNumber.Length !== 16) {
+		if (!this.modulus) {
+			throw new Error('modulus not defined')
+		}
+		if (accountNumber.length !== 16) {
 			throw new Error(`Invalid Bank Account Format '${formattedAccountNumber}'`);
 		}
 
 		let total = 0;
-		for (let i = 0; i < accountNumber.Length; i++) {
-			const number = Number.parseInt(accountNumber.Substring(i, 1));
+		for (let i = 0; i < accountNumber.length; i++) {
+			const value = +accountNumber.substring(i, 1);
 			const weighting = this.weightings[i];
-			const weightedValue = number * weighting;
+			const weightedValue = value * weighting;
 			total += weightedValue;
 		}
 		return total % this.modulus === 0;
 	}
 }
 
-export class AlgorithmA extends  Modulus11AlgorithmBase
-{
+export class AlgorithmA extends Modulus11AlgorithmBase {
 	_weightings: number[] = [0, 0, 6, 3, 7, 9, 0, 10, 5, 8, 4, 2, 1, 0, 0, 0];
 	_modulus: number = 11;
-	
+}
+export class AlgorithmD extends Modulus11AlgorithmBase {
+	_weightings: number[] = [0, 0, 0, 0, 0, 0, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0];
+	_modulus: number = 11;
+}
+export class AlgorithmE extends Modulus11AlgorithmBase {
+	_weightings: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 4, 3, 2, 0, 0, 1];
+	_modulus: number = 11;
+}
+export class AlgorithmF extends Modulus11AlgorithmBase {
+	_weightings: number[] = [0, 0, 0, 0, 0, 0, 1, 7, 3, 1, 7, 3, 1, 0, 0, 0];
+	_modulus: number = 10;
+}
+export class AlgorithmG extends Modulus11AlgorithmBase {
+	_weightings: number[] = [0, 0, 0, 0, 0, 0, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1];
+	_modulus: number = 10;
+}
+export class AlgorithmX extends Modulus11AlgorithmBase {
+	_weightings: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+	_modulus: number = 1;
 }
